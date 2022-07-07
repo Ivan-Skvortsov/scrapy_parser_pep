eda95a01a2ffe420b1cbf207bac202a5eaef2b2e
Ivan Skvortsov <pprofcheg@gmail.com>
2022-07-07 12:13:25 +0300

    finished parser

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,23 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import csv
+from collections import defaultdict
 
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+from pep_parse.settings import PEP_STATUS_REPORT_FILE
 
 
 class PepParsePipeline:
+    """Counts PEP by statuses and store data in csv report file."""
+    def open_spider(self, spider):
+        self.pep_statuses = defaultdict(int)
+
     def process_item(self, item, spider):
+        self.pep_statuses[item['status']] += 1
         return item
+
+    def close_spider(self, spider):
+        total = sum(self.pep_statuses.values())
+        with open(PEP_STATUS_REPORT_FILE, 'w', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['Статус', 'Количество'])
+            for status in self.pep_statuses.items():
+                writer.writerow(status)
+            writer.writerow(['Total', total])
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -6,11 +6,13 @@ from pep_parse.items import PepParseItem
 
 
 class PepSpider(scrapy.Spider):
+    """Parse peps.python.org, get all PEP data: number, name, status."""
     name = 'pep'
     allowed_domains = ['peps.python.org']
     start_urls = ['http://peps.python.org']
 
     def parse(self, response):
+        """Parse main page, get all links to all PEP pages."""
         numerical_index_table = response.css('section[id="numerical-index"]')
         pep_links = numerical_index_table.css(
             'a.pep.reference.internal::attr("href")'
@@ -19,12 +21,13 @@ class PepSpider(scrapy.Spider):
             yield response.follow(link, callback=self.parse_pep)
 
     def parse_pep(self, response):
+        """Parse single PEP page, get PEPs number, name and status."""
         pep_title = response.css('h1.page-title::text').get()
         pattern = re.compile(r'PEP\s(?P<pep_number>\d+)\s–\s(?P<pep_name>.+)')
         num_and_name = re.search(pattern, pep_title)
         status = response.css('dt:contains("Status") + dd::text').get()
         yield PepParseItem(
-            number=int(num_and_name.group('pep_number').strip()),
-            name=num_and_name.group('pep_name').strip(),
+            number=int(num_and_name.group('pep_number')),
+            name=num_and_name.group('pep_name'),
             status=status
         )
